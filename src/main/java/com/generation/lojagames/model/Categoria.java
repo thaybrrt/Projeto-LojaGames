a29bd2e4affe75a677862e0215b34a9495b0d554
thaybrrt <thaybarreto@live.com>
2024-01-17 18:24:52 -0300

    terceiro commit

--- src/main/java/com/generation/lojagames/model/Categoria.java
+++ src/main/java/com/generation/lojagames/model/Categoria.java
@@ -6,9 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.math.BigDecimal;
 
 @Entity
 @Table(name="tb_categoria")
@@ -19,16 +17,12 @@ public class Categoria {
 	private long id;
 	
 	@NotBlank(message="Nome não pode estar vazio")
-	@Size(min=6, max=50, message="Nome deve conter entre 6 e 50 caracteres")
+	@Size(min=2, max=50, message="Nome deve conter entre 2 e 50 caracteres")
 	private String nome;
 	
 	@NotBlank(message="Descrição nunca pode estar vazia")
-	@Size(min=10, max=100, message="Descrição deve ter entre 10 e 100 caracteres")
+	@Size(min=5, max=100, message="Descrição deve ter entre 5 e 100 caracteres")
 	private String descricao;
-
-    // Alteração no tipo do campo nome para BigDecimal
-    @NotNull(message = "Preço não pode ser nulo")
-    private BigDecimal preco;
 
 	public long getId() {
 		return id;
@@ -52,15 +46,5 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
-	}
-
-    // Getters e setters para o campo preco
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
     }
 }
